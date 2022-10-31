61a634af41a968b1c9bc4db3fb52353a66049d68
Tango2Tiger <clementkrasmussen@gmail.com>
2022-10-31 12:46:32 +0100

    Create SQL intro lab.sql

---
+++ SQL intro lab.sql
@@ -0,0 +1,19 @@
+
+
+select * from sakila.actor;
+select * from sakila.city;
+
+select title from sakila.film;
+
+
+select name as language from sakila.language;
+
+select * from sakila.store;
+# They have two stores
+
+select * from sakila.staff;
+# Two employees as well
+
+select first_name from sakila.staff;
+# Mike and John
+
